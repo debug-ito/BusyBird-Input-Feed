e9037650d81e90a38aeef269779f342191e3aeb3
Toshio Ito <debug.ito@gmail.com>
2014-07-07 22:10:00 +0900

    writing POD.

--- lib/BusyBird/Input/Feed.pm
+++ lib/BusyBird/Input/Feed.pm
@@ -11,13 +11,75 @@ __END__
 
 =head1 NAME
 
-BusyBird::Input::Feed - abstract
+BusyBird::Input::Feed - input BusyBird statuses from RSS/Atom feed
 
 =head1 SYNOPSIS
 
+    use BusyBird;
+    use BusyBird::Input::Feed;
+    
+    my $input = BusyBird::Input::Feed->new;
+    
+    my $statuses = $input->parse($feed_xml);
+    timeline("feed")->add($statuses);
+    
+    $statuses = $input->parse_file("feed.atom");
+    timeline("feed")->add($statuses);
+    
+    $statuses = $input->parse_url('https://metacpan.org/feed/recent?f=');
+    timeline("feed")->add($statuses);
+
 =head1 DESCRIPTION
 
+L<BusyBird::Input::Feed> converts RSS and Atom feeds into L<BusyBird> status objects.
+
+=head1 CLASS METHODS
+
+=head2 $input = BusyBird::Input::Feed->new(%args)
+
+The constructor.
+
+Fields in C<%args> are:
+
+=over
+
+=item C<use_favicon> => BOOL (optional, default: true)
+
+TODO: write this.
+
+=back
+
+=head1 OBJECT METHODS
+
+=head2 $statuses = $input->parse($feed_xml_string)
+
+=head2 $statuses = $input->parse_string($feed_xml_string)
+
+TODO: write this. no auto-guessing input type... no Filehandle type.
+
+=head2 $statuses = $input->parse_file($feed_xml_filename)
+
+TODO:
+
+=head2 $statuses = $input->parse_url($feed_xml_url)
+
+=head2 $statuses = $input->parse_uri($feed_xml_url)
+
+TODO:
+
 =head1 SEE ALSO
+
+=over
+
+=item *
+
+L<BusyBird>
+
+=item *
+
+L<BusyBird::Manual::Status>
+
+=back
 
 =head1 REPOSITORY
 
