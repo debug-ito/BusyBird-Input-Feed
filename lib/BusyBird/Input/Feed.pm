2975dc958eac5eb7cb37bdb900ea78835440ce2b
Toshio Ito <debug.ito@gmail.com>
2014-07-21 14:09:32 +0900

    Add parallel.pl example.

--- lib/BusyBird/Input/Feed.pm
+++ lib/BusyBird/Input/Feed.pm
@@ -204,6 +204,47 @@ Same as C<parse_string()> except C<parse_url()> downloads the feed XML from C<$f
 
 C<parse_uri()> method is an alias for C<parse_url()>.
 
+=head1 EXAMPLE
+
+The example below uses L<Parallel::ForkManager> to parallelize C<parse_url()> method of L<BusyBird::Input::Feed>.
+It greatly reduces the total time to download a lot of RSS/Atom feeds.
+
+    use strict;
+    use warnings;
+    use Parallel::ForkManager;
+    use BusyBird::Input::Feed;
+    use open qw(:std :encoding(utf8));
+    
+    my @feeds = (
+        'https://metacpan.org/feed/recent?f=',
+        'http://www.perl.com/pub/atom.xml',
+        'https://github.com/perl-users-jp/perl-users.jp-htdocs/commits/master.atom',
+    );
+    my $MAX_PROCESSES = 10;
+    my $pm = Parallel::ForkManager->new($MAX_PROCESSES);
+    my $input = BusyBird::Input::Feed->new;
+    
+    my @statuses = ();
+    
+    $pm->run_on_finish(sub {
+        my ($pid, $exitcode, $id, $signal, $coredump, $statuses) = @_;
+        push @statuses, @$statuses;
+    });
+    
+    foreach my $feed (@feeds) {
+        $pm->start and next;
+        warn "Start loading $feed\n";
+        my $statuses = $input->parse_url($feed);
+        warn "End loading $feed\n";
+        $pm->finish(0, $statuses);
+    }
+    $pm->wait_all_children;
+    
+    foreach my $status (@statuses) {
+        print "$status->{user}{screen_name}: $status->{text}\n";
+    }
+
+
 =head1 SEE ALSO
 
 =over
